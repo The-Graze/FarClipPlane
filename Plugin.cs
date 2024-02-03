ca3431977b6d4013978d9adb1a62e966f76ebe0e
BrokenSt0ne <105749252+BrokenSt0ne@users.noreply.github.com>
2024-02-03 23:37:48 +0000

    added src
    real!!!!

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace FarClipPlane
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void FixedUpdate()
+        {
+            Camera.main.farClipPlane = 25000;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace FarClipPlane
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.brokenstone.gorillatag.farclipplane";
+        public const string Name = "FarClipPlane";
+        public const string Version = "1.0.0";
+    }
+}
