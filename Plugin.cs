3df0228461a0525c009d8adcac8de15715116ed0
Graze <me@graze.cc>
2025-03-11 14:08:18 +0000

    wawa

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using GorillaNetworking;
 using System;
 using UnityEngine;
 using Utilla;
@@ -8,12 +9,21 @@ namespace FarClipPlane
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
+        bool init;
+        void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(delegate { CosmeticsV2Spawner_Dirty.OnPostInstantiateAllPrefabs2 += delegate { init = true; }; });
+        }
         public void FixedUpdate()
         {
-            foreach(Camera camera in Camera.allCameras)
+            if (init)
             {
-                camera.farClipPlane = float.MaxValue;
-                camera.clearFlags = CameraClearFlags.Skybox;
+                foreach (Camera camera in Camera.allCameras)
+                {
+                    camera.farClipPlane = float.MaxValue -1;
+                    camera.nearClipPlane = 0.001f;
+                    camera.clearFlags = CameraClearFlags.Skybox;
+                }
             }
         }
     }
